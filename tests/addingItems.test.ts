30576824e19c03e2a5359125fda7b347b656835c
Joanna Liana <joanna_liana@outlook.com>
2023-01-28 12:51:15 +0100

    Implement basic adding items

--- tests/addingItems.test.ts
+++ tests/addingItems.test.ts
@@ -1,30 +1,65 @@
+import { format } from 'date-fns';
+import parse from 'date-fns/parse';
+
+const DATE_FORMAT = 'dd/MM/yyyy';
+
 describe('Adding items to smart fridge', () => {
-  let itemsInFridge: unknown;
+  interface ItemToAdd {
+    name: string;
+    expiry: string;
+  }
 
-  // TODO: scenarios
-  // empty fridge
-  // fridge with items in it
-  // duplicate item? prevent to ensure correct calculation of expiry date
-  it('when an item is added, the fridge captures information about that item', () => {
-    // given, when
-    setCurrentDate('18/10/2021');
-    itemAdded({ name: 'Milk', expiry: '21/10/21' });
+  interface ItemInFridge {
+    name: string;
+    expiry: string;
+    // TODO: this should be stored as a date, string formatting only for display
+    addedAt: string;
+  }
 
-    setCurrentDate('01/01/2022');
-    itemAdded({ name: 'Cheese', expiry: '18/01/22' });
+  class SmartFridge {
+    constructor(public readonly itemsInFridge: ItemInFridge[]) {}
 
-    // then
-    expect(fridgeItems).toEqual([
-      { name: 'Milk', expiry: '21/10/21', addedAt: '18/10/2021' },
-      { name: 'Cheese', expiry: '18/11/21', addedAt: '01/01/2022' },
-    ]);
+    async addItem(item: ItemToAdd) {
+      this.itemsInFridge.push({
+        ...item,
+        addedAt: format(new Date(), DATE_FORMAT)
+      });
+    }
+  }
+
+  let fridge: SmartFridge;
+
+  beforeAll(() => {
+    fridge = new SmartFridge([]);
+    jest.useFakeTimers();
   });
+
+  it(
+    'when an item is added, the fridge captures information about that item',
+    () => {
+      // given, when
+      setCurrentDate('18/10/2021');
+      itemAdded({ name: 'Milk', expiry: '21/10/21' });
+
+      setCurrentDate('01/01/2022');
+      itemAdded({ name: 'Cheese', expiry: '18/01/22' });
+
+      // then
+      expect(fridge.itemsInFridge).toEqual([
+        { name: 'Milk', expiry: '21/10/21', addedAt: '18/10/2021' },
+        { name: 'Cheese', expiry: '18/01/22', addedAt: '01/01/2022' },
+      ]);
+    }
+  );
+
+  function setCurrentDate(_payload: string) {
+    const date = parse(_payload, DATE_FORMAT, new Date());
+
+    jest.setSystemTime(date);
+  }
+
+  function itemAdded(_payload: { name: string; expiry: string }) {
+    fridge.addItem(_payload);
+  }
 });
 
-function setCurrentDate(_payload: string) {
-  throw new Error('Function not implemented.');
-}
-
-function itemAdded(_payload: { name: string; expiry: string }) {
-  throw new Error('Function not implemented.');
-}
