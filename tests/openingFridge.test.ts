f2f8fa0dd642bde2740e57ba60e2d848fe5e597c
Joanna Liana <joanna_liana@outlook.com>
2023-04-09 07:22:50 +0200

    Adjust test

--- tests/openingFridge.test.ts
+++ tests/openingFridge.test.ts
@@ -6,6 +6,7 @@ import {
 } from '../src/SmartFridge';
 import { setCurrentDate } from './utils';
 
+// TODO: set timezone, check with GH actions
 describe('Opening smart fridge', () => {
   let fridge: SmartFridge;
 
@@ -15,6 +16,8 @@ describe('Opening smart fridge', () => {
 
   beforeEach(() => {
     setCurrentDate('18/10/2021');
+
+    fridge = new SmartFridge();
   });
 
   describe(
@@ -30,24 +33,28 @@ describe('Opening smart fridge', () => {
           condition: 'sealed'
         };
 
-        fridge = new SmartFridge([
-          ItemAdded(sealedItem),
-        ]);
+        fridge.handle(ItemAdded(sealedItem));
+
+        expect(fridge.itemsInFridge).toEqual([{
+          addedAt: '2021-10-17T22:00:00.000Z',
+          expiry: '2021-10-21T22:00:00.000Z',
+          name: 'Bacon'
+        }]);
 
         // when, then
         fridgeDoorOpened();
 
         expect(fridge.itemsInFridge).toEqual([{
           addedAt: '2021-10-17T22:00:00.000Z',
-          expiry: '2021-10-21T23:00:00.000Z',
+          expiry: '2021-10-21T21:00:00.000Z',
           name: 'Bacon'
         }]);
 
         fridgeDoorOpened();
 
         expect(fridge.itemsInFridge).toEqual([{
-          addedAt: '2021-10-21T23:00:00.000Z',
-          expiry: '2021-10-21T22:00:00.000Z',
+          addedAt: '2021-10-17T22:00:00.000Z',
+          expiry: '2021-10-21T20:00:00.000Z',
           name: 'Bacon'
         }]);
       });
