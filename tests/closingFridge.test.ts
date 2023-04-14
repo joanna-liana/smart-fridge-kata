e64d57d86a0fed8910fd5540f576fd2abdfe3066
Joanna Liana <joanna_liana@outlook.com>
2023-04-14 20:52:54 +0200

    Add a failing test for closing the fridge

---
+++ tests/closingFridge.test.ts
@@ -0,0 +1,25 @@
+import { SmartFridge } from '../src/SmartFridge';
+import { FridgeTestActions, testActionsFor } from './utils';
+
+describe('Closing a smart fridge', () => {
+  let fridge: SmartFridge;
+  let actions: FridgeTestActions;
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  beforeEach(() => {
+    fridge = new SmartFridge();
+    actions = testActionsFor(fridge);
+
+  });
+
+  it('does not work on an already closed fridge', () => {
+    actions.fridgeDoorOpened();
+    actions.fridgeDoorClosed();
+
+    expect(() => actions.fridgeDoorClosed())
+      .toThrowError('Cannot close an already closed fridge');
+  });
+});
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,9 +1,26 @@
 import parse from 'date-fns/parse';
 
-import { DATE_FORMAT } from '../src/SmartFridge';
+import {
+  DATE_FORMAT,
+  FridgeDoorClosed,
+  FridgeDoorOpened,
+  SmartFridge
+} from '../src/SmartFridge';
 
 export function setCurrentDate(_payload: string) {
   const date = parse(_payload, DATE_FORMAT, new Date());
 
   jest.setSystemTime(date);
+}
+
+export interface FridgeTestActions {
+  fridgeDoorOpened: () => void;
+  fridgeDoorClosed: () => void;
+}
+
+export function testActionsFor(fridge: SmartFridge) {
+  return {
+    fridgeDoorOpened: () => fridge.handle(FridgeDoorOpened()),
+    fridgeDoorClosed: () => fridge.handle(FridgeDoorClosed())
+  };
 }
