0206eb4e6dbe2b72940b5a03045a6284cce09e95
Joanna Liana <joanna_liana@outlook.com>
2023-04-16 08:53:48 +0200

    Restructure tests for a nicer report with fewer duplications

--- tests/displayingItems.test.ts
+++ tests/displayingItems.test.ts
@@ -13,16 +13,18 @@ describe('Displaying the status of the items stored in a smart fridge', () => {
   })
 
   // TODO: decide how to display multiple expired items
-  it.each([
-    ["exactly the same expiry as the current date", new Date(2021, 0, 2)],
-    ["expiry before the current date", new Date(2021, 0, 1, 23, 59, 59)],
-  ])('shows EXPIRED for items past their expiry date - %s', (_scenario, expiry) => {
-    setCurrentDate('01/02/2021');
+  describe("shows EXPIRED for items past their expiry date", () => {
+    it.each([
+      ["exactly the same expiry as the current date", new Date(2021, 0, 2)],
+      ["expiry before the current date", new Date(2021, 0, 1, 23, 59, 59)],
+    ])('%s', (_scenario, expiry) => {
+      setCurrentDate('01/02/2021');
 
-    fridge = new SmartFridge([
-      item("Milk", expiry)
-    ]);
+      fridge = new SmartFridge([
+        item("Milk", expiry)
+      ]);
 
-    expect(fridge.display).toEqual(`EXPIRED: Milk`);
-  });
+      expect(fridge.display).toEqual(`EXPIRED: Milk`);
+    });
+  })
 });
