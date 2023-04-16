0a0100705467a24a6c01b98c5ef3c64078cb5206
Joanna Liana <joanna_liana@outlook.com>
2023-04-16 08:58:23 +0200

    Add test for displaying 0 days left

--- tests/displayingItems.test.ts
+++ tests/displayingItems.test.ts
@@ -12,19 +12,36 @@ describe('Displaying the status of the items stored in a smart fridge', () => {
     condition: 'sealed'
   })
 
+  beforeEach(() => {
+    setCurrentDate('01/02/2021');
+  })
+
   // TODO: decide how to display multiple expired items
   describe("shows EXPIRED for items past their expiry date", () => {
     it.each([
       ["exactly the same expiry as the current date", new Date(2021, 0, 2)],
       ["expiry before the current date", new Date(2021, 0, 1, 23, 59, 59)],
     ])('%s', (_scenario, expiry) => {
-      setCurrentDate('01/02/2021');
-
       fridge = new SmartFridge([
         item("Milk", expiry)
       ]);
 
       expect(fridge.display).toEqual(`EXPIRED: Milk`);
     });
+  })
+
+  describe("shows the days left until an item's expiry", () => {
+    describe("0 days for items expiring on the same day", () => {
+      it.each([
+        ["1 hour left", new Date(2021, 1, 2, 1)],
+        ["23 hours left", new Date(2021, 1, 2, 23)],
+      ])('%s', (_scenario, expiry) => {
+        fridge = new SmartFridge([
+          item("Milk", expiry)
+        ]);
+
+        expect(fridge.display).toEqual(`Milk: 0 days remaining`);
+      });
+    })
   })
 });
